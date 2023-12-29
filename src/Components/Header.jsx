1e236e4e70e3b88665f9ecc598daa46bd644cb2a
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-12-29 15:04:07 +0530

    cake

---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Navbar } from 'react-bootstrap'
+
+function Header() {
+  return (
+    <div>
+       <Navbar  className="bg-body-tertiary ">
+        <Container>
+          <Navbar.Brand href="#home">
+          <i class="fa-solid fa-cake-candles"></i>{' '}
+            CAKE WORLD
+          </Navbar.Brand>
+          <input style={{
+            
+
+          }} className='rounded' type="text" />
+          
+        </Container>
+        <h3 className='p-3 ' style={{fontSize:'20px',color:'black'}}>LOGOUT</h3>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
