acc073b5ea3eb99f80ac8bd5fa69ec381aa7f8f7
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2024-01-30 15:18:55 +0530

    card

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Header from './Components/Header';
 import Dashboard from './Pages/Dashboard'
 import Footer from './Components/Footer';
+import Cards from './Components/Cards';
 function App() {
 
   return (
 
     <>
       <Header />
-
+<Cards/>
       <Routes>
         <Route path='/' element={<Dashboard />} />
       </Routes>
